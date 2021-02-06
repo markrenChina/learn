a89d961f476c275c75b934aa93cad40b1afd41ae
markrenChina <390835144@qq.com>
2021-02-06 09:14:22 +0800

    add reactor

---
+++ reactor/src/test/java/com/ccand99/learnreactor/LearnReactorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ccand99.learnreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+class LearnReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println();
+	}
+
+}
