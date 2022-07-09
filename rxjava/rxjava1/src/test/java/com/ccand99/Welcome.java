b5a79c721da23bb3328d648d12e99e8dee23ce06
markrenChina <390835144@qq.com>
2022-07-09 22:16:04 +0800

    c run jvm

--- rxjava/rxjava1/src/test/java/com/ccand99/Welcome.java
+++ rxjava/rxjava1/src/test/java/com/ccand99/Welcome.java
@@ -5,11 +5,6 @@ public class Welcome {
     }
 
     public static void main(String[] args) {
-        String[] greeting = new String[]{"com.ccand99.Welcome to Core Java", "by Cay Horstmann", "and Gary Cornell"};
-
-        for (String g : greeting) {
-            System.out.println(g);
-        }
         var china = new China();
         china.printf();
     }
